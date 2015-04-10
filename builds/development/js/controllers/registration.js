fbb4139d0326be751590f4fb0c8ba6becb1fa1b8
Tuan Nguyen <mtuan93@gmail.com>
2015-04-09 21:18:56 -0400

    Add register form, add controllers for login and register events

---
+++ builds/development/js/controllers/registration.js
@@ -0,0 +1,10 @@
+myApp.controller('RegistrationController', function($scope, $location){
+	
+	$scope.login = function() {
+		$location.path('/meetings');
+	}//login
+
+	$scope.register = function() {
+		$location.path('/meetings');
+	}
+});//Registration Controller
