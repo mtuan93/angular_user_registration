aea04a28697f9c9be0a7d90ccd031c373335b14b
Tuan Nguyen <mtuan93@gmail.com>
2015-04-16 20:18:34 -0400

    Add checkin controller

--- builds/development/js/controllers/checkIns.js
+++ builds/development/js/controllers/checkIns.js
@@ -1,4 +1,25 @@
 myApp.controller('CheckInsController', function($scope, $rootScope, 
-	$firebase, $firebaseSimpleLogin, FIREBASE_URL) {
+	$firebase, $firebaseSimpleLogin, $routeParams, FIREBASE_URL) {
 
-}); //CheckIns Controller
+	$scope.whichmeeting = $routeParams.mId;	
+	$scope.whichuser = $routeParams.uId;
+	
+	var ref = new Firebase(FIREBASE_URL + '/users/' + $scope.whichuser + 
+			'/meetings/' + $scope.whichmeeting + '/checkins');
+
+	$scope.addCheckin = function() {
+		var checkinsObj = $firebase(ref);
+
+		var myData = {
+			firstname: $scope.user.firstname,
+			lastname: $scope.user.lastname,
+			email: $scope.user.email,
+			data: Firebase.ServerValue.TIMESTAMP
+		};// myData
+
+		checkinsObj.$push(myData).then(function() {
+
+		}); //data sent to firebase
+	};// addCheckin
+	
+}); //CheckIns Controller 
